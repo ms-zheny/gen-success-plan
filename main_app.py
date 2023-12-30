b49e5458f55cdb2a1e1012efae7825555d8fdcf7
zhen yuan <zhen.yuan@microsoft.com>
2023-12-31 08:33:38 +1100

    disable authentication

--- main_app.py
+++ main_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from msal_streamlit_authentication import msal_authentication
+from msal import PublicClientApplication
+import webbrowser
+import requests
 import urllib.request
 import json
 import os
@@ -10,50 +12,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-with st.sidebar:
-    st.write("## User Sign In/out:")
-    auth_data = msal_authentication(
-            auth={
-                "clientId":  os.getenv("CLIENT_ID"),
-                "authority": os.getenv("AUTHORITY"),
-                "redirectUri": "/",
-                "postLogoutRedirectUri": "/"
-            }, # Corresponds to the 'auth' configuration for an MSAL Instance
-            cache={
-                "cacheLocation": "sessionStorage",
-                "storeAuthStateInCookie": False
-            }, # Corresponds to the 'cache' configuration for an MSAL Instance
-            login_request={}, # Optional
-            logout_request={}, # Optional
-            login_button_text="Login", # Optional, defaults to "Login"
-            logout_button_text="Logout", # Optional, defaults to "Logout"
-            class_name="css_button_class_selector", # Optional, defaults to None. Corresponds to HTML class.
-            html_id="btnLogin", # Optional, defaults to None. Corresponds to HTML id.
-            key=1 # Optional if only a single instance is needed
-    )
-
-if not auth_data:
-    st.image('./assets/home.jpg', caption='Created by Zhen YUAN', use_column_width='auto')
-    st.stop()
-
-
-def allowSelfSignedHttps(allowed):
-    # bypass the server certificate verification on client side
-    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-        ssl._create_default_https_context = ssl._create_unverified_context
-
-
 tpid = st.text_input("TPID", help="Enter TPID")
 website = st.text_input("Website URL",help="Enter Customer's website URL")
-
-
 
 if tpid and website:
 
     #st.write("TPID: ", tpid)
     #st.write("Website: ", website)
-
-    allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
 
     data = {
         'TPID': tpid,
